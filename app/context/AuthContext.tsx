97bcc4449b456dede40333324335ec6e2f94f3a2
StarlessDev <contact@starless.dev>
2025-06-01 13:10:40 +0200

    clean: whoops 🫢

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -100,7 +100,6 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
   const refreshTask = () => {
     return retrieveUser().then((user) => {
-      console.log("dio porco")
       setUser(user);
     })
   }
